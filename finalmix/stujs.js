c8cc405039b8764c8cb28c4151ffce6f0a111c1c
SatwikPV <79073422+SatwikPV@users.noreply.github.com>
2022-01-27 12:46:21 +0530

    Update stujs.js

--- finalmix/stujs.js
+++ finalmix/stujs.js
@@ -1,2 +1,68 @@
-let trial_card = document.querySelectorAll('.card1-button');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=1280, initial-scale=1.0">
+    <title>Student Page</title>
+    <link href="stu.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="topbar">
+        <div class="logo">
+            <img src="bmslogo.png" width = "220px"alt="Logo" height="220px">
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li><a href="stu.html">Student Dashboard</a></li>
+                <li><a href="aboutpage.html">About us</a></li>
+                <li><a href="teachers.html">Teachers Dashboard</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="main">
+        <div class="notif">
+            <div class="notif-box">
+                <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo adipisci illum quaerat saepe. Nobis provident repellendus assumenda sunt ipsa incidunt. Culpa, dicta. Blanditiis ex dignissimos, beatae delectus laudantium optio voluptatibus.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid labore vel eveniet alias nemo fugit, voluptate debitis adipisci possimus consequuntur officiis vero soluta modi non hic consequatur. Est nemo atque facere unde eos aperiam temporibus quisquam necessitatibus reiciendis enim quas mollitia nulla consectetur earum et cupiditate delectus, aliquid laborum adipisci, nam sunt? Dignissimos voluptatibus aut veniam hic architecto exercitationem commodi.</li>
+
+                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum fugit nesciunt reprehenderit. Libero corporis atque totam dolorem minus quos eveniet saepe necessitatibus, consequatur facere laudantium ratione et eum ad culpa debitis vel animi impedit accusamus ducimus. Temporibus explicabo laborum blanditiis.this is a sample notiif</li>
+
+                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A assumenda in quo sit laboriosam vero, aliquid officia illo ipsa? Ipsum.this is a sample notiif</li>
+
+                   
+                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque nobis delectus sunt, tenetur pariatur doloribus, reprehenderit ullam magni quae, unde deleniti perferendis accusamus vero necessitatibus hic! Nisi eius beatae voluptates numquam quia mollitia. Laboriosam doloribus fuga possimus! Dolore, voluptatem ipsum.this is a sample notiif</li>
+                
+                </ul>
+
+                </div>
+        </div>
+
+
+
+        <div class="batches">
+            <div class="boxc1" id="box1">
+                <span>Batch1</span>
+                <button>Login</button>
+            </div>
+
+            <div class="boxc1" id="box2">
+                <span>Batch2</span>
+                <button formaction="signs.html">Login</button>
+            </div>
+
+            <div class="boxc1" id="box3">
+                <span>Batch3</span>
+                <button>Login</button>
+            </div>
+
+            <div class="boxc1" id="box4">
+                <span>Batch4</span>
+                <button>Login</button>
+            </div>
+
 
